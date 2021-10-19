e034a5fc8d3455bf2a7e9f9a00acfc3f44466a33
João Vítor Carvalho <mistingerr@gmail.com>
2021-10-19 11:11:24 -0300

    added excercise 3

---
+++ csharp-exercises_3/csharp-exercises_3/Program.cs
@@ -0,0 +1,53 @@
+﻿using csharp_exercises_3.Entities;
+using csharp_exercises_3.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace csharp_exercises_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(clientName, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order ?: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(productName, productValue);
+
+                Console.Write("Product quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(quantity, productValue, product);
+
+                order.AddItem(orderItem);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("ORDER SUMMARY: ");
+            Console.WriteLine(order);
+
+        }
+    }
+}
