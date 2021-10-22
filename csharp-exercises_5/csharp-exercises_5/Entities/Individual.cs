f1b3e4b1e0eaa557c68d06006bff060a96fb8400
João Vítor Carvalho <mistingerr@gmail.com>
2021-10-22 15:25:59 -0300

    added-exercise

---
+++ csharp-exercises_5/csharp-exercises_5/Entities/Individual.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exercises_5.Entities
+{
+    class Individual : TaxPayer
+    {
+        public double HealthExpeditures { get; set; }
+
+        public Individual(string name, double income, double healthExpeditures)
+               : base(name, income)
+        {
+            HealthExpeditures = healthExpeditures;
+        }
+
+        public override double Tax()
+        {
+            if (AnualIncome < 20000.0)
+            {
+                return AnualIncome * 0.15 - HealthExpeditures * 0.5;
+            }
+            else
+            {
+                return AnualIncome * 0.25 - HealthExpeditures * 0.5;
+            }
+        }
+    }
+}
