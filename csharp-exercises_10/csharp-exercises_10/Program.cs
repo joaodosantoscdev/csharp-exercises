61cd6a1f607541e68715fcc8d5ead7cce4e7b881
João Vítor Carvalho <mistingerr@gmail.com>
2021-10-29 15:48:04 -0300

    execise-added

---
+++ csharp-exercises_10/csharp-exercises_10/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace csharp_exercises_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            Console.Write("Enter the full file path: ");
+            string sourcePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                using (StreamReader sr = File.OpenText(sourcePath))
+                {
+                    while (!sr.EndOfStream) 
+                    {
+                        string[] votingRecord = sr.ReadLine().Split(',');
+                        string candidate = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+
+                        if (dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                    }
+
+                    foreach (var item in dictionary)
+                    {
+                        Console.WriteLine($"{item.Key}, {item.Value}");
+                    }
+                }
+            } catch (IOException e) 
+            {
+                Console.WriteLine("An error ocurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
